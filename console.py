b2689e7bc0b15ee4b2e0bfff387b3eb646970617
johnkenzzy <johnkenumeh@gmail.com>
2024-11-14 05:30:20 -0800

    Coded all basic interpreter commands

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ Class HBNBCommand is defined in this module and inherits from cmd.Cmd
 """
 import cmd
 import sys
+import shlex
 from models.base_model import BaseModel
 from models import storage
 
@@ -32,7 +33,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         class_name = args[0]
-        if class_name != "BaseModel":
+        if class_name not in ['BaseModel']:
             print("** class doesn't exist **")
             return
 
@@ -52,17 +53,17 @@ class HBNBCommand(cmd.Cmd):
             return
 
         class_name = args[0]
-        class_id = args[1] if len(args) > 1 else None
+        instance_id = args[1] if len(args) > 1 else None
 
-        if class_name != "BaseModel":
+        if class_name not in ['BaseModel']:
             print("** class doesn't exist **")
             return
         
-        if not class_id:
+        if not instance_id:
             print("** instance id missing **")
             return
 
-        key = f"{class_name}.{class_id}"
+        key = f"{class_name}.{instance_id}"
         instance = storage.all().get(key)
         
         if instance:
@@ -71,7 +72,10 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_destroy(self, line):
-        """Deletes an instance based on the class"""
+        """Deletes an instance based on the class
+
+            Ex: destroy BaseModel 1234-1234-1234
+        """
         args = line.split()
 
         if not args:
@@ -79,13 +83,13 @@ class HBNBCommand(cmd.Cmd):
             return
 
         class_name = args[0]
-        class_id = args[1] if len(args) > 1 else None
+        instance_id = args[1] if len(args) > 1 else None
 
-        if class_name != "BaseModel":
+        if class_name not in ['BaseModel']:
             print("** class doesn't exist **")
             return
         
-        if not class_id:
+        if not instance_id:
             print("** instance id missing **")
             return
 
@@ -98,7 +102,7 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_all(self, line):
-        """Prints all string representation of all instances"""
+        """Prints all string representation of all instances of a class"""
         args = line.split()
 
         if not args:
@@ -106,7 +110,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         class_name = args[0]
-        if class_name != "BaseModel":
+        if class_name not in ['BaseModel']:
             print("** class doesn't exist **")
             return
         
@@ -119,6 +123,67 @@ class HBNBCommand(cmd.Cmd):
 
         print(filtered_objs)
 
+    def do_update(self, line):
+        """Updates an instance based on the class name and id
+
+            Ex: update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        """
+        args = shlex.split(line)
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in ['BaseModel']:
+            print("** class doesn't exist **")
+            return
+
+        instance_id = args[1] if len(args) > 1 else None
+        if not instance_id:
+            print("** instance id missing **")
+            return
+
+        all_objs = storage.all()
+        obj_key = f"{class_name}.{instance_id}"
+        if obj_key not in all_objs:
+            print("** no instance found **")
+            return
+
+        attr_name = args[2] if len(args) > 2 else None
+        if not attr_name:
+            print("** attribute name missing **")
+            return
+        
+        attr_value = args[3] if len(args) > 3 else None
+        if not attr_value:
+            print("** value missing **")
+            return
+
+        obj = all_objs[obj_key]
+
+        if attr_name in ("id", "created_at", "updated_at"):
+            print("** attribute cannot be updated **")
+            return
+
+        current_value = getattr(obj, attr_name, None)
+        if isinstance(current_value, int):
+            try:
+                attr_value = int(attr_value)
+            except ValueError:
+                print("** invalid value type **")
+                return
+        elif isinstance(current_value, float):
+            try:
+                attr_value = float(attr_value)
+            except ValueError:
+                print("** invalid value type **")
+                return
+        elif isinstance(current_value, str):
+            attr_value = str(attr_value)
+
+        setattr(obj, attr_name, attr_value)
+        obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
